6f50d50fe287381a6f480e26ecae11feccddc62f
Ron Gierlach <rongierlach@yahoo.com>
2014-04-11 16:48:43 -0500

    added name field and publish button to survey create/update form

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,5 +1,19 @@
 enable :sessions
 
 get '/create' do 
-
+  @header = "Create"
+  @name = "Name Your Survey"
+  erb :'survey/create'
 end
+
+get '/update/:survey_id' do
+  @header = "Update"
+  @survey = Survey.find(params[:survey_id])
+  name = @survey.name
+  questions = @survey.questions
+  erb :'survey/create'
+end
+
+# post '' do
+
+# end

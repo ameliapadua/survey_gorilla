8f1aec274b55e43228400715ff99720b7c9ef0a7
Amelia Padua <amelia.padua@gmail.com>
2014-04-13 14:46:27 -0500

    Added view and javascript to fill out a survey

--- public/js/application.js
+++ public/js/application.js
@@ -5,21 +5,52 @@ $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 
-//   var choice = $('.choice')
 
-//   $('.submit').click(function(event){
-//      event.preventDefault();
-
-//      $.post('/surveys/:id', function(data) {
-//         $( ".result" ).html( data );
-//      });
-//   });  
  });
 
 
 
+function Survey(surveyJson, complete) {
+  this.surveyJson = surveyJson;
+  this.id = surveyJson.survey.id;
+  this.title = surveyJson.survey.title;
+  this.questionIndex = 0;
+  this.questions = surveyJson.survey.questions;
+  this.complete = complete;
+}
 
+Survey.prototype.displayTitle = function(selector) {
+  $(selector).append(this.title)
+}
 
-// $.post( "ajax/test.html", function( data ) {
-//   $( ".result" ).html( data );
-// });
+Survey.prototype.displayQuestion = function(question_selector, choices_selector) {
+  var self = this;
+  var currentQuestion = self.questions[self.questionIndex]
+
+  $(question_selector).empty();
+  $(choices_selector).empty();
+
+  $(question_selector).append(currentQuestion.content);
+
+  for (var i=0; i < currentQuestion.choices.length; i++) {
+    $(choices_selector).append("<input type='radio' name=" + currentQuestion.id + " value=" + currentQuestion.choices[i].id + " />" + currentQuestion.choices[i].content + "<br>");
+  }
+
+  $('#choices input').click(function(e){
+    e.preventDefault();
+
+    var choice_data = {choice_id: this.value}
+
+    $.post('/surveys/' + self.id, choice_data, function(response){
+      if (self.questionIndex < self.questions.length) {
+        self.questionIndex++;
+        self.displayQuestion(question_selector, choices_selector);
+      }
+      else{
+        alert("We've reached the end!");
+      }
+    });
+
+  });
+
+}
